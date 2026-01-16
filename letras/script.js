928cb24a0fe1e773f6ae52387bdf19ad4d87cb24
RocchiaHUB <zueiratotalizada31@gmail.com>
2026-01-16 11:20:14 -0300

    script.js update covers

--- letras/script.js
+++ letras/script.js
@@ -4,157 +4,157 @@ const musicas = [
   {
     title: "Burro Caubói",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/BC.jpg",
+    capa: "../covers-letras/BC.jpg",
     mega: "https://drive.google.com/file/d/1QzcK-xR4slEZiTAooN2x9yCz9PmPdc5C/view?usp=sharing" // ← substitua pelo link real do Mega
   },
   {
     title: "Cabrão do Destino",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/CDD.jpg",
+    capa: "../covers-letras/CDD.jpg",
     mega: "https://drive.google.com/file/d/193lkDF9-Jlt__DYLq9vDmiASaBtMVo1A/view?usp=sharing"
   },
   {
     title: "Cala Boca, Glerba!",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/CBG.jpg",
+    capa: "../covers-letras/CBG.jpg",
     mega: "https://drive.google.com/file/d/1N4NRYnFuKDb0ZTT-1EgvnnO0T_73JqJG/view?usp=sharing"
   },
   {
     title: "Corno de Novo",
     artist: "deevmusics",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/CDN.jpg",
+    capa: "../covers-letras/CDN.jpg",
     mega: "https://drive.google.com/file/d/164VrYUnLmYpYr6rel9cxsDWX0o17RNzB/view?usp=sharing"
   },
   {
     title: "Dicionário de Palavrão",
     artist: "arthurn",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/DP.jpg",
+    capa: "../covers-letras/DP.jpg",
     mega: "https://drive.google.com/file/d/1bxYBadlNFMNhE4WWxC-oPsz3COSOPiaZ/view?usp=sharing"
   },
   {
     title: "Foto na Estante",
     artist: "arthurn",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/FNE.jpg",
+    capa: "../covers-letras/FNE.jpg",
     mega: "https://drive.google.com/file/d/1BGsXPuqo0qaX4q6AecaKejGZSNX52lUR/view?usp=drive_link"
   },
   {
     title: "Fundão dos Fracassados",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/FDF.jpg",
+    capa: "../covers-letras/FDF.jpg",
     mega: "https://drive.google.com/file/d/1oAQXgL24Eo4smpR-Oy2SN0Jv-ahEVFj5/view?usp=drive_link"
   },
   {
     title: "Golfe Branquinho",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/GB1.jpeg",
+    capa: "../covers-letras/GB1.jpeg",
     mega: "https://drive.google.com/file/d/1JTNowKISkZw0m-A_JeAqK6rg949P9rym/view?usp=drive_link"
   },
   {
     title: "Golfe Branco",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/GB2.jpeg",
+    capa: "../covers-letras/GB2.jpeg",
     mega: "https://drive.google.com/file/d/1dW6sR1Q1BnDPZufORncikHzIv4wi7VhG/view?usp=drive_link"
   },
   {
     title: "Hotel Galinhas",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/HG.jpg",
+    capa: "../covers-letras/HG.jpg",
     mega: "https://drive.google.com/file/d/1fPmRSh5gso0JezlBIYZO7rigmczRhbHO/view?usp=drive_link"
   },
   {
     title: "Hulk do Galo",
     artist: "arthurn",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/HUG.jpg",
+    capa: "../covers-letras/HUG.jpg",
     mega: "https://drive.google.com/file/d/1VCR9qm6IzDVWSRgZaP3irqnhFVm-CNvI/view?usp=drive_link"
   },
   {
     title: "Julietto",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/J.jpg",
+    capa: "../covers-letras/J.jpg",
     mega: "https://drive.google.com/file/d/1iLq9lHpBEiK8eHAqm_M9maxcFlNBEYqk/view?usp=drive_link"
   },
   {
     title: "Lua",
     artist: "arthurn",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/L.jpg",
+    capa: "../covers-letras/L.jpg",
     mega: "https://drive.google.com/file/d/1kGmjF0M2Y_Cw7mzE7jXpnaycjCT1SdFR/view?usp=drive_link"
   },
   {
     title: "Lua de Paçoca",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/LP.jpg",
+    capa: "../covers-letras/LP.jpg",
     mega: "https://drive.google.com/file/d/11ST4m3m0I1WSY-C2mKsjF_qJXtaRn42G/view?usp=drive_link"
   },
   {
     title: "Magazine Estranha",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/ME.jpg",
+    capa: "../covers-letras/ME.jpg",
     mega: "https://drive.google.com/file/d/1GeNqi8gO4ryGKUnrAXOpOdwsa6EPpeq3/view?usp=drive_link"
   },
   {
     title: "Malé",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/ML.jpg",
+    capa: "../covers-letras/ML.jpg",
     mega: "https://drive.google.com/file/d/1gXxEdiFEBHd324M-wK3gkCL-2_7Ml4Vm/view?usp=drive_link"
   },
   {
     title: "Mendigo do Tigrinho",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/MD.jpg",
+    capa: "../covers-letras/MD.jpg",
     mega: "https://drive.google.com/file/d/1UZk-LGHP8ROFLoo_hXzDAX2qJ4LoYghZ/view?usp=drive_link"
   },
   {
     title: "Mercado de Calcinha",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/MC.jpg",
+    capa: "../covers-letras/MC.jpg",
     mega: "https://drive.google.com/file/d/1m5pHD6Cjqqlpq6OZ2C_jolndTctWSwto/view?usp=drive_link"
   },
   {
     title: "Palavrão Day",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/PD.jpg",
+    capa: "../covers-letras/PD.jpg",
     mega: "https://drive.google.com/file/d/1O_exRyY_6y4qOmylbs3AF65OkxZ3oUxY/view?usp=drive_link"
   },
   {
     title: "Phonk do Dino",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/PDD.jpg",
+    capa: "../covers-letras/PDD.jpg",
     mega: "https://drive.google.com/file/d/14wseTdWJuLNB9ULtK4yIEtsrbEYx3Lk5/view?usp=drive_link"
   },
   {
     title: "Pocotó na Fazenda",
     artist: "deevmusics",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/PNF.jpg",
+    capa: "../covers-letras/PNF.jpg",
     mega: "https://drive.google.com/file/d/18OAYugAkE4PI_DGCxsxCeMBiVbpjSuai/view?usp=drive_link"
   },
   {
     title: "Porra Day",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/PRD.jpg",
+    capa: "../covers-letras/PRD.jpg",
     mega: "https://drive.google.com/file/d/1D7GS1UfGpMPwwgB9eIO_RGXEnl5l0OBy/view?usp=drive_link"
   },
   {
     title: "Primo Irritante",
     artist: "rothurmiusek",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/PI.jpg",
+    capa: "../covers-letras/PI.jpg",
     mega: "https://drive.google.com/file/d/10HeO4aBLytMosjT-ZhCh3cJQEHbwTJXN/view?usp=drive_link"
   },
   {
     title: "Thur M Puta Mentirosa",
     artist: "arthurn",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/TMPM.jpg",
+    capa: "../covers-letras/TMPM.jpg",
     mega: "https://drive.google.com/file/d/13H25DCmeVcKc2S86eQ0DuAzQAkI79Eam/view?usp=drive_link"
   },
   {
     title: "Vida da Thuremi",
     artist: "arthurn",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/VT.jpg",
+    capa: "../covers-letras/VT.jpg",
     mega: "https://drive.google.com/file/d/1FEkfWZIPoKC3vzqG-24dj_OSRybDJFcE/view?usp=drive_link"
   },
   {
     title: "Vida Merda",
     artist: "arthurn",
-    capa: "C:/Users/CLIENTE/Downloads/deevchadoloads/covers-letras/VM.jpg",
+    capa: "../covers-letras/VM.jpg",
     mega: "https://drive.google.com/file/d/1tt0CCRNnqT6zV9chpQ_nRIGn10CgYvsx/view?usp=drive_link"
   }
 ];
@@ -265,3 +265,4 @@ if (toggleBtn && trackList) {
 document.addEventListener("DOMContentLoaded", () => {
   renderizarMusicas();
 });
+
